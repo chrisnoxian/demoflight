2fc195ccf845dc984cb15dcdb538bdd8e427d140
Chris Stewart <31616719+chrisnoxian@users.noreply.github.com>
2017-12-12 16:13:23 +0530

    Update demo1.php

--- demo1.php
+++ demo1.php
@@ -155,43 +155,8 @@ Locate the flotation device, typically beneath your seat.
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
   <span class="error">* <?php echo $genderErr;?></span>
-  
-  </form>
-<?php
-require("PHPMailerAutoload.php");
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = "mail.smtp2go.com";;  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'gameboydrewy@gmail.com';                 // SMTP username
-    $mail->Password = 'vegetachris93';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 2525;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom('stewchris0293@gmail.com', 'Mailer');
-    $mail->addAddress('gameboydrewy@gmail.com', 'chris stewart');     // Add a recipient
-    
-    
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'ticket confirmation';
-    $mail->Body    = 'your ticket is been confirmed<b>and verified</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-      echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-}
-?>
-
-
-<form id="emailForm" name="emailForm" method="post" action="" >
+    </form>
+<form id="emailForm" name="emailForm" method="post" action="mail.php" >
 <table width="100%" border="0" align="center" cellpadding="2" cellspacing="1">
 <tr>
   <td colspan="4"><strong>Verification E-mail</strong></td>
